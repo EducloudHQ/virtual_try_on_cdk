f0d17bb81daccb51c65a42575b08e6709e6ae0fd
trey-rosius <treyrosius@gmail.com>
2025-08-07 19:14:51 +0100

    Initial commit

---
+++ tests/unit/test_virtual_tryon_cdk_stack.py
@@ -0,0 +1,15 @@
+import aws_cdk as core
+import aws_cdk.assertions as assertions
+
+from virtual_tryon_cdk.virtual_tryon_cdk_stack import VirtualTryonCdkStack
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in virtual_tryon_cdk/virtual_tryon_cdk_stack.py
+def test_sqs_queue_created():
+    app = core.App()
+    stack = VirtualTryonCdkStack(app, "virtual-tryon-cdk")
+    template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
